f017c1b2317179b3f0aa45f5146b7b98831d7c64
tekelia-powe <tekelia.powe@bryanuniversity.edu>
2020-09-22 22:34:20 -0500

    Week1 Assignments

---
+++ week1/todo-app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+function App() {
+  return (
+    <div>
+      <input type="checkbox"/><p>Pack for Vacation</p>
+      <input type="checkbox" /><p>Complete Homework for FSW-120</p>
+      <input type="checkbox"/><p>Pick up incidental from Grocery store</p>
+    </div>
+  );
+}
+
+export default App;
